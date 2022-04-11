4ef554638da7b23df50da1d5a173b2eb55371850
dhruvi <shahdhruvi1512@gmail.com>
2022-04-11 20:45:52 +0530

    added project management backend

--- project/backend/controllers/projectManagementController.js
+++ project/backend/controllers/projectManagementController.js
@@ -17,30 +17,14 @@ class projectManagement{
                 console.log(err);
                 response.status(400).send({message: "Project could not be added. Please try again."});
               } else {
-                console.log("New user inserted");
+                console.log("New project inserted");
                 response.status(200).send({message: "New Project Added"});
               }
             }
         );  
     }
 
-    viewProject = (request, response) => {
-        let {projectName,deptCode,users,product,status,cieAreaId,financeProductId} = request.body;
-        client.query(
-            `insert into projects (projectName,deptCode,users,product,status,cieAreaId,financeProductId) values($1,$2,$3,$4,$5,$6,$7)`, 
-            [projectName,deptCode,users,product,status,cieAreaId,financeProductId],
-            (err, resp) => {
-              // console.log(resp);
-              if (err) {
-                console.log(err);
-                response.status(400).send({message: "Project could not be added. Please try again."});
-              } else {
-                console.log("New user inserted");
-                response.status(200).send({message: "New Project Added"});
-              }
-            }
-        );  
-    }
+    
 
 
 }
